adc5038a69dd085d480f9afee12f4b323c70857e
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2023-01-27 10:35:26 +0300

    lab done

---
+++ src/components/GifList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function GifList({dataFetch}) {
+  const returnedImages = dataFetch && dataFetch.map((data, index) => (
+    <li key={index}>
+      <img src={data.images.original.url}/>
+    </li>
+  ))
+    return (
+      <ul>
+        {returnedImages}
+      </ul>
+    )
+}
+
+export default GifList;
